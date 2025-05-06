615f75af060946df6cc0147a0ec093cdfb674a09
Haoyang Tan <jeff.tandev@gmail.com>
2025-05-05 17:10:44 -0700

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+import Searchbar from "../Searchbar/Searchbar";
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Logo</div>
+      <div className="nav-link">
+        <a href="#">Location</a>
+        <Searchbar />
+        <a href="#">Account</a>
+        <a href="#">Cart</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
