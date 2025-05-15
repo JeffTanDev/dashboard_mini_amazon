a178c3febead8e76bce94f1ffd887250f4ac2266
Haoyang Tan <jeff.tandev@gmail.com>
2025-05-14 17:50:00 -0700

    account drop down

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,12 @@ import "./Navbar.css";
 import Searchbar from "../Searchbar/Searchbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const [username, setUsername] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("token");
     const getUsername = async () => {
@@ -25,6 +28,12 @@ function Navbar() {
     };
     if (token) getUsername();
   }, []);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    setUsername("");
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">Logo</div>
@@ -32,7 +41,51 @@ function Navbar() {
         <a href="#">Location</a>
         <Searchbar />
         {username ? (
-          <div> Hello {username} </div>
+          <div
+            className="user-menu"
+            onMouseEnter={() => setShowDropdown(true)}
+            onMouseLeave={() => setShowDropdown(false)}
+          >
+            <div className="username">
+              <span className="hello-text">Hello，{username}</span>
+              <span className="account-text">Account & List ▾</span>
+            </div>
+            {showDropdown && (
+              <div className="dropdown-menu">
+                <div className="dropdown-header">
+                  <button
+                    className="signin-button"
+                    onClick={() => navigate("/profile")}
+                  >
+                    Manage My Account
+                  </button>
+                </div>
+                <div className="dropdown-content">
+                  <div className="dropdown-section">
+                    <h4>Account</h4>
+                    <ul>
+                      <li onClick={() => navigate("/profile")}>Account Info</li>
+                      <li onClick={() => navigate("/orders")}>Orders</li>
+                      <li onClick={() => navigate("/address")}>Address</li>
+                      <li onClick={() => navigate("/payments")}>Payment</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-section">
+                    <h4>Setting</h4>
+                    <ul>
+                      <li onClick={() => navigate("/notifications")}>
+                        Notify Setting
+                      </li>
+                      <li onClick={() => navigate("/security")}>
+                        Data Security
+                      </li>
+                      <li onClick={handleLogOut}>Log Out</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         ) : (
           <a href="/login">Account</a>
         )}
